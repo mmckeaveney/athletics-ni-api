bd4cb3cc60c0af73f3a573704f93876c64bd522c
Martin McKeaveney <martin@squarefoot.com>
2018-12-11 14:56:44 +0000

    rearranging folder structure

--- api/src/index.js
+++ api/src/index.js
@@ -19,30 +19,31 @@ async function parsePdfText(pdf) {
   return page.getTextContent();
 }
 
-function buildRaceObjects(pdfStrings) {
-  const result = [];
-  let raceArr = [];
-  pdfStrings.forEach((pdfString, index) => {
-    const nextString = pdfStrings[index + 1];
-    const nextNextString = pdfStrings[index + 2];
-    if (nextString === undefined || nextNextString === undefined) return;
-    const nextTwoEmpty = !nextString.trim() && !nextNextString.trim();
-    if (nextTwoEmpty) {
-      result.push(raceArr);
-      raceArr = [];
-    } else {
-      raceArr.push(pdfString);
-    }
-  })
-  return result;
-}
+//function buildRaceObjects(pdfStrings) {
+  //const result = [];
+  //let raceArr = [];
+  //pdfStrings.forEach((pdfString, index) => {
+    //const nextString = pdfStrings[index + 1];
+    //const nextNextString = pdfStrings[index + 2];
+    //if (nextString === undefined || nextNextString === undefined) return;
+    //const nextTwoEmpty = !nextString.trim() && !nextNextString.trim();
+    //if (nextTwoEmpty) {
+      //result.push(raceArr);
+      //raceArr = [];
+    //} else {
+      //raceArr.push(pdfString);
+    //}
+  //})
+  //return result;
+//}
 
 async function parsePdf(pdf) {
   const textContent = await parsePdfText(pdf);
+  console.log(textContent)
   const raceItems = textContent.items.slice(RACES_START_INDEX);
   const pdfStrings = raceItems.map(pdfItem => pdfItem.str);
-  const raceObjects = buildRaceObjects(pdfStrings);
-  console.log(raceObjects);
+  //const raceObjects = buildRaceObjects(pdfStrings);
 }
 
 pdfjs.getDocument({ url: PDF_PATH }).then(parsePdf);
+
