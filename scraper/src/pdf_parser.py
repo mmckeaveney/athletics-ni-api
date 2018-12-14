21b2aa90df79a921f2a0688f226f7d2ddf69ebdc
Martin McKeaveney <martinmckeaveney@gmail.com>
2018-12-14 00:18:48 +0000

    fix dockerfile to allow poetry package management

--- scraper/src/pdf_parser.py
+++ scraper/src/pdf_parser.py
@@ -2,12 +2,14 @@ import tabula
 import logging
 
 logging.basicConfig(level=logging.INFO)
+
 URL = "https://athleticsni.org/download/files/"
 
 def parse_pdf_to_csv(pdf_file_name):
     logging.info("Fetching PDF..")
-    data_frame = tabula.read_pdf(URL + pdf_file_name)
+    data_frame = tabula.read_pdf(URL + pdf_file_name, pages="all")
     print(data_frame.to_string())
     logging.info(pdf_file_name + " parsed successfully.")
+    data_frame.to_csv("foo.csv")
     
 parse_pdf_to_csv("XCandRRFixtures-Updated-131218.pdf")
